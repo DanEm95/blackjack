d8b89087c86310fc224b05544a92a1001a2abfe9
DanEm95 <110663668+DanEm95@users.noreply.github.com>
2024-12-28 14:28:56 +0100

    Improvements

--- main.py
+++ main.py
@@ -4,37 +4,38 @@
 # There are no jokers.
 # The Jack/Queen/King all count as 10.
 # The the Ace can count as 11 or 1.
-# Use the following list as the deck of cards:
-# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-# The cards in the list have equal probability of being drawn.
-# Cards are not removed from the deck as they are drawn.
+# Use the following list as the deck of CARDS:
+# CARDS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+# The CARDS in the list have equal probability of being drawn.
+# CARDS are not removed from the deck as they are drawn.
 
 import random
 import os
 from art import logo
+import cProfile
 
+CARDS = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
     """Returns a random card from the deck."""
-    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    card = random.choice(cards)
-    return card
+    return random.choice(CARDS)
 
 
-def calculate_score(cards):
-    """Take a list of cards and return the score calculated from the cards"""
-    if sum(cards) == 21 and len(cards) == 2:
-        return 0
-    if 11 in cards and sum(cards) > 21:
-        cards.remove(11)
-        cards.append(1)
-    return sum(cards)
+
+def calculate_score(CARDS):
+    """Take a list of CARDS and return the score calculated from the CARDS"""
+    score = sum(CARDS)
+    if score == 21 and len(CARDS) == 2:
+        return 21
+    
+    if 11 in CARDS and score > 21:
+        score -= 10
+    return score
 
 
 def compare(user_score, computer_score):
     if user_score > 21 and computer_score > 21:
         return "You went over. You lose 😤"
-
     if user_score == computer_score:
         return "Draw 🙃"
     elif computer_score == 0:
@@ -54,18 +55,14 @@ def compare(user_score, computer_score):
 def play_game():
     print(logo)
 
-    user_cards = []
-    computer_cards = []
+    user_cards = [deal_card(), deal_card()]
+    computer_cards = [deal_card(), deal_card()]
     is_game_over = False
 
-    for _ in range(2):
-        user_cards.append(deal_card())
-        computer_cards.append(deal_card())
-
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
 
     while not is_game_over:
-        user_score = calculate_score(user_cards)
-        computer_score = calculate_score(computer_cards)
         print(f"   Your cards: {user_cards}, current score: {user_score}")
         print(f"   Computer's first card: {computer_cards[0]}")
 
@@ -75,6 +72,7 @@ def play_game():
             user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
             if user_should_deal == "y":
                 user_cards.append(deal_card())
+                user_score = calculate_score(user_cards)  # Update score
             else:
                 is_game_over = True
 
@@ -87,6 +85,9 @@ def play_game():
     print(compare(user_score, computer_score))
 
 
+
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    # TODO
+    #cProfile.run('play_game()')
     os.system('cls')
     play_game()
